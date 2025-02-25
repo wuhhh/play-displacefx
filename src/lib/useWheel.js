f0a38020d48e8626bd1a3c80df66afa6b145d425
Huw Roberts <huw@huwroberts.dev>
2025-02-25 23:49:11 +0000

    Updated useWheel, provides more data

--- src/lib/useWheel.js
+++ src/lib/useWheel.js
@@ -1,21 +1,32 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const useWheel = () => {
-  const [wheelDelta, setWheelDelta] = useState(0);
+  const [wheelData, setWheelDelta] = useState({
+    deltaY: 0,
+    ternary: 0,
+    timestamp: 0,
+    wheelDeltaY: 0,
+  });
 
   useEffect(() => {
     const handleWheel = event => {
-      setWheelDelta(event.wheelDeltaY);
+      let { deltaY, timestamp, wheelDeltaY } = event;
+      setWheelDelta({
+        deltaY,
+        ternary: deltaY == 0 ? deltaY : deltaY > 0 ? 1 : -1,
+        timestamp,
+        wheelDeltaY,
+      });
     };
 
-    window.addEventListener("wheel", handleWheel);
+    window.addEventListener("wheel", handleWheel, false);
 
     return () => {
       window.removeEventListener("wheel", handleWheel);
     };
   }, []);
 
-  return wheelDelta;
+  return wheelData;
 };
 
 export default useWheel;
